6e28a913595d3d08d3e2e304c9cf8842ba902c0b
Lucas Parada <lucasparada20@gmail.com>
2025-08-12 10:41:48 -0400

    Update run_heur.sh

--- run_heur.sh
+++ run_heur.sh
@@ -3,7 +3,7 @@
 EXEC_FILE="build/exec_heur"
 
 if [[ -x "$EXEC_FILE" ]]; then
-    "$EXEC_FILE" instance_file=instances/quebec166_20.txt
+    "$EXEC_FILE" instance_file=instances/quebec167_20_SW.txt
 else
     echo "Error: Executable '$EXEC_FILE' not found or not executable. Did you build it first ... ?"
     exit 1
