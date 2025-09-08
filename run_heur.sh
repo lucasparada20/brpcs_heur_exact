ca6700b668cd5f2cf64352588458589d66517bfe
Lucas Parada <lucasparada20@gmail.com>
2025-09-08 10:48:04 -0400

    Update run_heur.sh

--- run_heur.sh
+++ run_heur.sh
@@ -3,7 +3,7 @@
 EXEC_FILE="build/exec_heur"
 
 if [[ -x "$EXEC_FILE" ]]; then
-    "$EXEC_FILE" instance_file=instances/quebec167_20_SW.txt construction_heuristic=SEQ
+    "$EXEC_FILE" instance_file=instances/quebec167_20_CS.txt construction_heuristic=SEQ max_route_distance=60
 else
     echo "Error: Executable '$EXEC_FILE' not found or not executable. Did you build it first ... ?"
     exit 1
